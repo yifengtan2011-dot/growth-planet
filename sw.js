8df4e194222499aa9abc657929396335e7fd05cd
yifengtan2011-dot <yifeng.tan2011@gmail.com>
2026-07-28 21:24:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='growth-planet-v1';const ASSETS=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
